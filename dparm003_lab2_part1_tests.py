1159cf3488ed8f280bce33cbbafd98065e86699d
Dhruv Parmar <56183031+P-Dhruv20@users.noreply.github.com>
2021-04-09 12:41:02 -0700

    Update dparm003_lab2_part1_tests.py

--- dparm003_lab2_part1_tests.py
+++ dparm003_lab2_part1_tests.py
@@ -1,13 +1,4 @@
-#The time should be a multiple of
-#           the period of the system
-#       *iterations - The number of clock ticks to wait (periods)
-#       expected - The expected value at the end of this step (after the "time" has elapsed.) 
-#           If this value is incorrect the test will fail early before completing.
-#       * only one of these should be used
-#   expected - The expected output (as a list of tuples) at the end of this test
-# An example set of tests is shown below. It is important to note that these tests are not "unit tests" in 
-# that they are not ran in isolation but in the order shown and the state of the device is not reset or 
-# altered in between executions (unless preconditions are used).
+
 tests = [ {'description': 'PINA: 0x00 => PORTB: 0x00',
     'steps': [ {'inputs': [('PINA',0x00)], 'iterations': 5 } ],
     'expected': [('PORTB',0x00)],
@@ -26,9 +17,3 @@ tests = [ {'description': 'PINA: 0x00 => PORTB: 0x00',
     },
     ]
 #watch = ['PORTB']
-
-
-# Optionally you can add a set of "watch" variables these need to be global or static and may need
-# to be scoped at the function level (for static variables) if there are naming conflicts. The 
-# variables listed here will display everytime you hit (and stop at) a breakpoint
-
