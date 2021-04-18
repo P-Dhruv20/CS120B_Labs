1e5aef7e116331c3ef20d6e97eecaa10063c1c3e
Dhruv Parmar <56183031+P-Dhruv20@users.noreply.github.com>
2021-04-18 16:06:32 -0700

    Update dparm003_lab4_part1_tests.py

--- dparm003_lab4_part1_tests.py
+++ dparm003_lab4_part1_tests.py
@@ -4,6 +4,17 @@ tests = [ {'description': 'PINA: 0x01, 0x00, 0x01 => PORTB: 0x01',
         {'inputs': [('PINA', 0x01)], 'iterations': 5}],
     'expected': [('PORTB',0x01)],
     },
+	{'description': 'PINA: 0x00, 0x01, 0x01 => PORTB: 0x02',
+    'steps': [{'inputs': [('PINA', 0x00)], 'iterations': 5},
+        {'inputs': [('PINA', 0x01)], 'iterations': 5},
+        {'inputs': [('PINA', 0x01)], 'iterations': 5}],
+    'expected': [('PORTB',0x02)],
+    },
+	{'description': 'PINA: 0x00, 0x00, 0x01 => PORTB: 0x01',
+    'steps': [{'inputs': [('PINA', 0x00)], 'iterations': 5},
+        {'inputs': [('PINA', 0x00)], 'iterations': 5},
+        {'inputs': [('PINA', 0x01)], 'iterations': 5}],
+    'expected': [('PORTB',0x01)],
+    },
     ]
 #watch = ['state']
-
