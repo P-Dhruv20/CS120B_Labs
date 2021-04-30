ea8caeb326955a26d7afd25efa44f42ed57cdb76
Dhruv Parmar <56183031+P-Dhruv20@users.noreply.github.com>
2021-04-30 00:22:01 -0700

    Add files via upload

---
+++ dparm003_lab6_part1.c
@@ -0,0 +1,70 @@
+/* Author: dparm003
+ * Lab Section: 22
+ * Assignment: Lab 6  Exercise 1
+ * Exercise Description: [optional - include for your own benefit]
+ *
+ * I acknowledge all content contained herein, excluding template or example
+ * code is my own original work.
+ *
+ *  Demo Link: 
+ */
+
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+enum SM_States { SM_Start, SM_PB0, SM_PB1, SM_PB2 } SM_State;
+
+void ToggleLights() {
+unsigned char tmpB;
+   switch ( SM_State ) {
+      case SM_Start:
+         SM_State = SM_PB0; 
+         break;
+	  case SM_PB0:
+		 SM_State = SM_PB1;
+		 break;
+	  case SM_PB1:
+		 SM_State = SM_PB2;
+		 break;
+	  case SM_PB2:
+		 SM_State = SM_PB0;
+		 break;
+      default:
+         SM_State = SM_Start;
+         break;
+   }
+
+   switch ( SM_State ) {
+      case SM_Start:
+         break;
+	  case SM_PB0:
+		 tmpB = 0x01;
+		 break;
+	  case SM_PB1:
+		 tmpB = 0x02;
+		 break;
+	  case SM_PB2:
+		 tmpB = 0x04;
+		 break;
+      default:
+         break;
+   }
+}
+
+void main() {
+	
+	DDRB = 0xFF; PORTB = 0x00;
+
+	unsigned char tmpB = 0x00;
+
+   TimerSet(1000);
+   TimerOn(); 
+   SM_States = SM_Start;
+   while (1) {         
+      tmpB = ToggleLights();
+      while (!TimerFlag){} 
+      TimerFlag = 0;     
+   }
+}
