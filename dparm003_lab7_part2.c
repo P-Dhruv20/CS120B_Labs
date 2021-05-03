a553cbf1210961812403f5df1c97b4da75fbff77
Dhruv Parmar <56183031+P-Dhruv20@users.noreply.github.com>
2021-05-02 20:46:13 -0700

    Lab 7 Part 2

---
+++ dparm003_lab7_part2.c
@@ -0,0 +1,48 @@
+/* Author: dparm003
+ * Lab Section: 22
+ * Assignment: Lab 7  Exercise 2
+ * Exercise Description: [optional - include for your own benefit]
+ *
+ * I acknowledge all content contained herein, excluding template or example
+ * code is my own original work.
+ *
+ *  Demo Link: 
+ */
+
+// MAX = 254
+// MIN = 62
+
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+void ADC_init(){
+	ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
+}
+
+int main(void) {
+   
+	DDRD = 0xFF; PORTD = 0x00;
+	DDRB = 0xFF; PORTB = 0x00;
+	DDRA = 0x00; PORTA = 0xFF;
+
+	unsigned char tmpD = 0x00;
+	unsigned char tmpB = 0x00;
+	unsigned short tmpA = 0x00;
+  	unsigned short myADC = 0x00;
+	
+	ADC_init();
+	
+	while (1) {
+	tmpA = PINA & 0xFF;
+	myADC = ADC;
+	tmpB = (char)myADC;  
+	tmpD = (char)(myADC >> 8); 
+	
+	PORTD = tmpD;
+	PORTB = tmpB;
+    }
+
+    return 1;
+}
